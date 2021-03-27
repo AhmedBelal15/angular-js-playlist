4ea44c52003fd2c559aac344279cb5cf403b4b9a
ahmedbelal15 <ahmbelal95@gmail.com>
2021-03-27 21:27:05 +0200

    filters

--- app/app.js
+++ app/app.js
@@ -1,5 +1,27 @@
 const firstModule = angular.module('firstModule', [])
 
-firstModule.controller('firstController', ($scope)=>{
+firstModule.controller('firstController', ['$scope', ($scope)=>{
     $scope.message = 'Hellooooooo from scope'
-})
+    $scope.favAnimeCharacters=[
+        {
+            name: 'Luffy',
+            gender: 'male',
+            superpower: 'Devil Fruit',
+            power: 100
+        },
+        {
+            name: 'Killua',
+            gender: 'male',
+            superpower: 'Nen',
+            power: 80
+        },
+        {
+            name: 'Mikasa',
+            gender: 'female',
+            superpower: 'Ackerman Superpower',
+            power: 2000
+        }
+    ]
+}]
+
+)
